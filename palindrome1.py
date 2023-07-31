19f52503810db1d225b44163131c78e143af8038
MEENAQuest <135314500+MEENAQuest@users.noreply.github.com>
2023-07-31 12:33:44 +0530

    Add files via upload

---
+++ palindrome1.py
@@ -0,0 +1,20 @@
+my_string = input("Enter a string: ")
+my_string_lower = my_string.lower()
+len_string = len(my_string_lower)
+
+i=0
+j=len_string - 1
+
+for i in range(len_string):
+    if my_string_lower[i] == my_string_lower[j]:
+        i=i+1
+        j=j-1
+        flag=1
+    else:
+        flag=0
+        break
+
+if flag==1:
+    print("Palindrome")
+if flag==0:
+    print("Not a Palindrome")
